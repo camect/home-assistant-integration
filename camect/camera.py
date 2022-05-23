a989df5e2751746ab8b578271aa16eb950509d07
Peter Epley <epleypa@outlook.com>
2022-05-22 23:10:25 -0400

    Update camera.py
    Update camera.py to fix #11

--- camect/camera.py
+++ camect/camera.py
@@ -91,7 +91,9 @@ class Camera(camera.Camera):
         """Return a link to the camera feed as entity picture."""
         return None
 
-    def camera_image(self):
+    def camera_image(
+        self, width = None, height = None
+    ):
         """Return a still image response from the camera."""
         return self._home.snapshot_camera(self._device_id)
 
